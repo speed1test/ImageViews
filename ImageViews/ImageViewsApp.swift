73abf907503660e43c19b7a1478c2825226a6328
Edwin <edwin12sv@gmail.com>
2023-12-28 23:19:10 -0600

    Initial Commit

---
+++ ImageViews/ImageViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageViewsApp.swift
+//  ImageViews
+//
+//  Created by elsalvador on 28/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
